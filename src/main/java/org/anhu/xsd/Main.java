26f640925460cc8cf3abf5979de5a50660ded343
s167710 <s167710@s167710.campus.tue.nl>
2018-08-16 11:29:44 +0200

    added comment

--- src/main/java/org/anhu/xsd/Main.java
+++ src/main/java/org/anhu/xsd/Main.java
@@ -10,24 +10,29 @@ public class Main {
 
 	public static void main(String args[]) throws ParserConfigurationException, SAXException, IOException,
 			ClassNotFoundException, InstantiationException, IllegalAccessException, ClassCastException {
-//		final String dir = "C:\\Users\\310160231\\Documents\\dps-css\\trunk\\software\\shared\\xsd";
-//		final String dir = "C:\\Users\\s167710\\git\\xsd-chopper\\target\\xsd_example";
-		final String dir = "C:\\Users\\anneke.huijsmans\\eclipse-workspace\\xsd-chopper\\target\\xsd_example";
+		final String dir = "C:\\Users\\310160231\\Documents\\dps-css\\trunk\\software\\shared\\xsd";
+		// final String dir =
+		// "C:\\Users\\s167710\\git\\xsd-chopper\\target\\xsd_example";
+		// final String dir =
+		// "C:\\Users\\anneke.huijsmans\\eclipse-workspace\\xsd-chopper\\target\\xsd_example";
 		Informant sherlock = new Informant(dir);
 
-//		System.out.println(sherlock.reportUnIncludedFiles());
-//		System.out.println(sherlock.reportDoubleFiles());
-//		System.out.println(sherlock.reportUnusedSimpleAndComplexTypes());
-//		System.out.println(sherlock.reportFilesWithIdenticalInhoud());
-//		System.out.println(sherlock.reportAllElementsWithFile());
-//		System.out.println(sherlock.reportAllDataTypesFromIncludedXSDThatAreUsedExactlyOnce());
-//
-//		final String dirdir = "C:\\Users\\anneke.huijsmans\\eclipse-workspace\\xsd-chopper\\target";
-//		final String file = "FilesWithATopLevelElement.txt";
-//		sherlock.reportAndPrintAllFilesWithTopLevelElement(dirdir, file);
+		// System.out.println(sherlock.reportUnIncludedFiles());
+		// System.out.println(sherlock.reportDoubleFiles());
+		// System.out.println(sherlock.reportUnusedSimpleAndComplexTypes());
+		// System.out.println(sherlock.reportFilesWithIdenticalInhoud());
+		// System.out.println(sherlock.reportAllElementsWithFile());
+		// System.out.println(sherlock.reportAllDataTypesFromIncludedXSDThatAreUsedExactlyOnce());
+		//
+		// final String dirdir =
+		// "C:\\Users\\anneke.huijsmans\\eclipse-workspace\\xsd-chopper\\target";
+		// final String file = "FilesWithATopLevelElement.txt";
+		// sherlock.reportAndPrintAllFilesWithTopLevelElement(dirdir, file);
 
-//		final String fileName = "BloSchema.xsd";
-//		sherlock.reportSingleXSDToFile(fileName);
+		// fill in the name of the xsd you want the inhoud off, report will apear in
+		// target folder and is called singleFileReport.txt
+		final String fileName = "BloSchema.xsd";
+		sherlock.reportSingleXSDToFile(fileName);
 
 		// output file can be found in target -> FileReport.txt
 		// specify in dirdir2 the directory of the file containing the files
